b9a62f5c1a8ad407bf65da7f017df00ed38ddcde
Hyoyoung CHANG <hyoyoung@gmail.com>
2014-07-03 14:10:36 +0900

    customize fishshell cmd prompt

--- config.fish
+++ config.fish
@@ -1,5 +1,53 @@
+function prompt_pwd --description 'Print the current working directory, NOT shortened to fit the prompt'
+        if test "$PWD" != "$HOME"
+                printf "%s" (echo $PWD|sed -e 's|/private||' -e "s|^$HOME|~|")
+        else
+                echo '~'
+        end
+
+end
+
+function fish_prompt --description 'Write out the prompt'
+
+        # Just calculate these once, to save a few cycles when displaying the prompt
+        if not set -q __fish_prompt_hostname
+                set -g __fish_prompt_hostname (hostname|cut -d . -f 1)
+        end
+
+        if not set -q __fish_prompt_normal
+                set -g __fish_prompt_normal (set_color normal)
+        end
+
+        switch $USER
+
+                case root
+
+                if not set -q __fish_prompt_cwd
+                        if set -q fish_color_cwd_root
+                                set -g __fish_prompt_cwd (set_color $fish_color_cwd_root)
+                        else
+                                set -g __fish_prompt_cwd (set_color normal)
+                        end
+                end
+
+                echo -n -s "$USER" @ "$__fish_prompt_hostname" ':' "$__fish_prompt_cwd" (prompt_pwd) "$__fish_prompt_normal" '# '
+
+                case '*'
+
+                if not set -q __fish_prompt_cwd
+                        set -g __fish_prompt_cwd (set_color normal)
+                end
+
+                echo -n -s "$USER" @ "$__fish_prompt_hostname" ':' "$__fish_prompt_cwd" (prompt_pwd) "$__fish_prompt_normal" '$ '
+
+        end
+end
+
     # path
     set -x PATH /usr/local/bin $PATH
+
+    # editor
+    set -x EDITOR vim
 
     # cmd prompt
     #export PS1="\u@\h:\w\$ "
