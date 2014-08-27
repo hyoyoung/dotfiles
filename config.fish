6ac3762fffaae1cf7862565b1f246ed194c793a5
Hyoyoung CHANG <hyoyoung@gmail.com>
2014-08-27 17:43:35 +0900

    remove color at error return code in fish config

--- config.fish
+++ config.fish
@@ -61,7 +61,7 @@ function fish_prompt --description 'Write out the prompt'
                 set -l prompt_status
                 if test $last_status -ne 0
                         if not set -q __fish_prompt_status
-                                set -g __fish_prompt_status (set_color $fish_color_status)
+                                set -g __fish_prompt_status (set_color normal)
                         end
                         set prompt_status "$__fish_prompt_status [$last_status]$__fish_prompt_normal"
                 end
