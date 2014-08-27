ea5ae38646e8172977186e11efedd66bf40eec25
Hyoyoung CHANG <hyoyoung@gmail.com>
2014-08-27 17:41:04 +0900

    add git branch in prompt to fish config

--- config.fish
+++ config.fish
@@ -9,6 +9,8 @@ end
 
 function fish_prompt --description 'Write out the prompt'
 
+        set -l last_status $status
+
         # Just calculate these once, to save a few cycles when displaying the prompt
         if not set -q __fish_prompt_hostname
                 set -g __fish_prompt_hostname (hostname|cut -d . -f 1)
@@ -18,10 +20,30 @@ function fish_prompt --description 'Write out the prompt'
                 set -g __fish_prompt_normal (set_color normal)
         end
 
+        if not set -q -g __fish_classic_git_functions_defined
+                set -g __fish_classic_git_functions_defined
+                function __fish_repaint_user --on-variable fish_color_user --description "Event handler, repaint when fish_color_user changes"
+                        if status --is-interactive
+                                set -e __fish_prompt_user
+                                commandline -f repaint ^/dev/null
+                        end
+                end
+                function __fish_repaint_host --on-variable fish_color_host --description "Event handler, repaint when fish_color_host changes"
+                        if status --is-interactive
+                                set -e __fish_prompt_host
+                                commandline -f repaint ^/dev/null
+                        end
+                end
+                function __fish_repaint_status --on-variable fish_color_status --description "Event handler; repaint when fish_color_status changes"
+                        if status --is-interactive
+                                set -e __fish_prompt_status
+                                commandline -f repaint ^/dev/null
+                        end
+                end
+        end
+
         switch $USER
-
                 case root
-
                 if not set -q __fish_prompt_cwd
                         if set -q fish_color_cwd_root
                                 set -g __fish_prompt_cwd (set_color $fish_color_cwd_root)
@@ -31,15 +53,20 @@ function fish_prompt --description 'Write out the prompt'
                 end
 
                 echo -n -s "$USER" @ "$__fish_prompt_hostname" ':' "$__fish_prompt_cwd" (prompt_pwd) "$__fish_prompt_normal" '# '
-
                 case '*'
-
                 if not set -q __fish_prompt_cwd
                         set -g __fish_prompt_cwd (set_color normal)
                 end
 
-                echo -n -s "$USER" @ "$__fish_prompt_hostname" ':' "$__fish_prompt_cwd" (prompt_pwd) "$__fish_prompt_normal" '$ '
+                set -l prompt_status
+                if test $last_status -ne 0
+                        if not set -q __fish_prompt_status
+                                set -g __fish_prompt_status (set_color $fish_color_status)
+                        end
+                        set prompt_status "$__fish_prompt_status [$last_status]$__fish_prompt_normal"
+                end
 
+                echo -n -s "$USER" @ "$__fish_prompt_hostname" ':' "$__fish_prompt_cwd" (prompt_pwd) (__fish_git_prompt) "$__fish_prompt_normal" "$prompt_status" '$ '
         end
 end
 
