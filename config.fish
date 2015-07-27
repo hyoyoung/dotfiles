58b75e53cb76e020f2534912e4a878a77a75462e
Hyoyoung CHANG <hyoyoung@gmail.com>
2015-07-27 14:07:32 +0900

    remove bold color in fish config

--- config.fish
+++ config.fish
@@ -10,7 +10,7 @@ end
 set -g __fish_git_prompt_show_informative_status 1
 set -g __fish_git_prompt_hide_untrackedfiles 1
 
-set -g __fish_git_prompt_color_branch magenta bold
+set -g __fish_git_prompt_color_branch magenta
 set -g __fish_git_prompt_showupstream "informative"
 set -g __fish_git_prompt_char_upstream_ahead "↑"
 set -g __fish_git_prompt_char_upstream_behind "↓"
@@ -26,7 +26,7 @@ set -g __fish_git_prompt_color_dirtystate blue
 set -g __fish_git_prompt_color_stagedstate yellow
 set -g __fish_git_prompt_color_invalidstate red
 set -g __fish_git_prompt_color_untrackedfiles $fish_color_normal
-set -g __fish_git_prompt_color_cleanstate normal bold
+set -g __fish_git_prompt_color_cleanstate normal
 
 function fish_prompt --description 'Write out the prompt'
 
