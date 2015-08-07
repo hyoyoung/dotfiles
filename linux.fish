1021b46e934d45dc71b985d343aefec50b278bd2
Hyoyoung CHANG <hyoyoung@gmail.com>
2015-08-08 00:31:06 +0900

    add new colorconfig.fish

--- linux.fish
+++ linux.fish
@@ -4,12 +4,7 @@
 # editor
 set -x EDITOR vim
 
-# cmd prompt
-#export PS1="\u@\h:\w\$ "
-
-# Tell ls to be colourful
-#set -x CLICOLOR 1
-#set -x LSCOLORS "ExGxBxDxCxEgEdxbxgacxd"
+set -x PS1 "\u@\h:\w\$ "
 
 # Tell grep to highlight matches
 #set -x GREP_OPTIONS '--color=auto'
@@ -17,4 +12,14 @@ set -x EDITOR vim
 # java dir path
 #set -x JAVA_HOME  (/usr/libexec/java_home)
 
-alias ls='ls -Fh'
+alias ls='ls --color=auto'
+
+alias grep='grep --color=auto'
+alias fgrep='fgrep --color=auto'
+alias egrep='egrep --color=auto'
+
+# some more ls aliases
+alias ll='ls -alF'
+alias la='ls -A'
+alias l='ls -CF'
+
