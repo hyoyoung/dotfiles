167eac645e86d7471d4ae63e299479476756be3c
Hyoyoung CHANG <hyoyoung@gmail.com>
2020-11-06 01:27:42 +0900

    add invert mouse button script

---
+++ mystartup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+sleep 10
+xmodmap ~/.Xmodmap
+for id in $(/usr/bin/xinput list | /bin/grep "USB Mouse\|Bluetooth.* Mouse" | /bin/grep -o id=[0-9]* | /bin/grep -o [0-9]*);
+do
+  echo "Inverted Mouse id is $id";
+  xinput set-button-map $id 3 2 1;
+done
