ed580bd3cb872d92013d6fb4ba6c3efc9228f7a3
Hyoyoung CHANG <hyoyoung@gmail.com>
2020-11-08 23:31:35 +0900

    add comments

--- mystartup.sh
+++ mystartup.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# awake from sleep, use '/usr/lib/systemd/system-sleep' dir to put a script
+
 sleep 10
 xmodmap ~/.Xmodmap
 for id in $(/usr/bin/xinput list | /bin/grep "USB Mouse\|Bluetooth.* Mouse" | /bin/grep -o id=[0-9]* | /bin/grep -o [0-9]*);
